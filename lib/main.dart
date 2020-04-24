baf0fdbe9422798b362c0ff77471f86a422c4a21
JustGinger888 <ethanmoffatt888@icloud.com>
2020-04-24 06:05:10 +0100

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,103 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          //ROW AND COLUMN ALL HAVE SAME RULES JUST FLIPPED X&Y VALUES
+          child: Column(
+            //verticalDirection: VerticalDirection.SPECIFY,
+            //mainAxisAlignment: MainAxisAlignment.SPECIFY,
+            //crossAxisAlignment: CrossAxisAlignment.SPECIFY,
+            //CAN CREATE INVISIBLE CONTAINER TO PLACE CHILDREN RIGHT USING INFINITY WIDTH
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/fox.jpg'),
+              ),
+              Text(
+                'Ethan Moffatt',
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontFamily: 'DrSugiyama'),
+              ),
+              Text(
+                'ASPIRING DEVELOPER',
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blue.shade800,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2),
+              ),
+              SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
+                      color: Colors.blue.shade800,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                      '+44 7828 41 4455',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                          fontFamily: 'SourceSansPro',
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                      'ethanmoffatt@gmail.com',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                          fontFamily: 'SourceSansPro',
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
